22968e264914c875ecc0e0c52d73e759bee10a84
rawoott <1105149@students.wits.ac.za>
2018-05-22 12:15:11 +0200

    AdditionalTests

--- test_classify_1.py
+++ test_classify_1.py
@@ -26,6 +26,22 @@ class TestCheck(unittest.TestCase):
         c1 = classify.thoseInRange(data,0,10)
         self.assertNotEqual(c1,["none"])
 
+    def test_Lab1(self):
+        data=[["A",0],["B",49],["C",59],["D",60],["E",69],["F",74],["H",100]]
+        c1 = classify.thoseInRange(data,70,75)
+        self.assertEqual(c1,[["F",74]])
+
+    def test_Lab2(self):
+        data=[["A",0],["B",49],["C",59],["D",60],["E",69],["F",74],["H",100]]
+        c1 = classify.thoseInRange(data,0,49)
+        self.assertEqual(c1,[["A",0]])
+
+    def test_Lab3(self):
+        data=[["A",0],["B",49],["C",59],["D",60],["E",69],["F",74],["H",100]]
+        c1 = classify.thoseInRange(data,61,69)
+        self.assertEqual(c1,["none"])
+
+
 
 if __name__ == '__main__':
 
